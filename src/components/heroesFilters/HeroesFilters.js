dcf088df325d9f53ec427a771015728be125cbe0
Maksim <128407703+SvetlanaVladislavovna@users.noreply.github.com>
2023-12-06 13:43:52 +0300

    thunk heroesFilters

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
 
-import {filtersFetching, filtersFetched, filtersFetchingError, activeFilterChanged} from '../../actions';
+import {fetchFilters, activeFilterChanged} from '../../actions';
 import  Spinner  from '../spinner/Spinner';
 
 
@@ -16,10 +16,8 @@ const HeroesFilters = () => {
     const {request} = useHttp();
 
     useEffect(()=>{
-        dispatch(filtersFetching());
-        request('http://localhost:3001/filters')
-                .then(data => dispatch(filtersFetched(data)))
-                .catch(() => dispatch(filtersFetchingError()))
+        dispatch(fetchFilters(request));
+       
     }, [])
 
     if (filtersLoadingStatus === 'loading') {
