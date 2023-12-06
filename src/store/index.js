814e150d4661b243e8bc8f4a0fac2a7108162055
Maksim <128407703+SvetlanaVladislavovna@users.noreply.github.com>
2023-12-06 11:48:53 +0300

    middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,20 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
+
+// эта функция принимает store  и возвращает другую функцию которая тоже автоматически будет подхватывать метод dispatch,  а эта функция в свою очередь будет возвращать еще одну функцию котоая как аргумент принимает action  который передается потом в dispatch
+// функция которая возвращается в конце это по факту и есть новая функция dispatch c измененным функционалом
+// store здесь это ={dispatch, getState}, здесь расположен не весь store а лишь две его сущности - это функция dispatch и метод getstate - этот метод нужен для того если вдруг внутри dispatch хотели получить какое то текущее значение state и что то с ним сделать 
+const stringMiddleware = (store)=>(next)=>(action)=>{
+    if(typeof action === 'string'){
+        return next({
+            type: action
+        })
+    }
+    return next(action)
+}
+// здесь мы сразу возвращаем функцию dispatch с новым функционалом и внутри мы говорим чтоо если нам action  приходит  в качестве строки то мы просто берем наш обычный dispatch и запускаем с эти объектом в котором строка будет выступать уже именно полем  
+
 
 // мы создаем новую переменную - она будет принимать createStore и возвращать новую функцию. внутри этой функции мы будем создавать store при помощи команды createstore()
 const enhancer = (createStore)=>(...args)=>{
@@ -27,10 +41,9 @@ const enhancer = (createStore)=>(...args)=>{
 // нас интересует оригинальная команда createStore  и в нее уже встроен механизм, что ечли мы сюда вторым аргументом передаем какую то функцию, то она является усилителем нашего store, то есть эта функция будет там запущена и она подменит оригинальный dispatch  и подставит тот функционал, который мы врусную прописали 
 const store = createStore(
                     combineReducers({heroes, filters}), 
-                    compose(
-                        enhancer,
-                        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                    ));
+                    compose(applyMiddleware(stringMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+                    
+                    );
 
 export default store;
 
