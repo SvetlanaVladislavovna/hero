27afa95d9e427fe475192686c2a651b29159b383
Maksim <128407703+SvetlanaVladislavovna@users.noreply.github.com>
2023-12-08 11:07:34 +0300

    filters slice

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import { heroesFetching, heroesFetched, heroesFetchingError } from "../components/heroesList/heroesSlice";
+import { filtersFetching, filtersFetched, filtersFetchingError } from "../components/heroesFilters/filterSlice";
 
 export const fetchHeroes = (request)=>(dispatch)=>{
     dispatch(heroesFetching());
@@ -18,32 +19,6 @@ export const fetchFilters = (request)=>(dispatch)=>{
 
 
 
-export const filtersFetching = () =>{
-    return {
-        type: 'FILTERS_FETCHING'
-    }
-}
-
-export const filtersFetched = (filters) =>{
-    return {
-        type: 'FILTERS_FETCHED',
-        payload: filters
-    }
-}
-
-export const filtersFetchingError =()=>{
-    return {
-        type: 'FILTERS_FETCHING_ERROR'
-    }
-}
-
-
-export const activeFilterChanged = (filter) =>{
-    return {
-        type: 'ACTIVE_FILTER_CHANGED',
-        payload: filter
-    }
-}
 
 
 
